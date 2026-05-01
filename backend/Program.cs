52d5a3b2fc58dfddca20497ea89d24e6a96f2854
Inge Bothma <ingesaunders@gmail.com>
2026-05-01 10:34:15 +0100

    initial full stack setup (backend, frontend, docker, postgres)

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,10 +1,12 @@
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Pipeline
