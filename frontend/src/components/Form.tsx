d8f4c4cbaea410ba3a55c4f016f8f2f1489dd6ba
Inge Bothma <ingesaunders@gmail.com>
2026-05-01 15:43:46 +0100

    Added useMutation to send api call to backend

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,21 +1,52 @@
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+
 type FormProps = {
   heading: string;
 };
 
 function Form({ heading }: FormProps) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const mutation = useMutation({
+    mutationFn: async (credentials: { username: string; password: string }) => {
+      console.log("here 1");
+      const result = await fetch("http://localhost:5200/api/tasks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+      });
+      return result.json();
+    },
+  });
+
+  function handleSubmit() {
+    mutation.mutate({
+      username,
+      password,
+    });
+  }
   return (
-    <>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit();
+      }}
+    >
       <h1>{heading}</h1>
       <label>
         Username
-        <input type="text" />
+        <input type="text" onChange={(e) => setUsername(e.target.value)} />
       </label>
       <label>
         Password
-        <input type="password" />
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
       </label>
-      <button>Submit</button>
-    </>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 export default Form;
